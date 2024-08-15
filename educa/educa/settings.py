68d8c6a6de8a12e1e272d7826dd1608ac6e72056
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-08-15 14:55:22 -0600

    strting bulding an API

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'embed_video',
     'debug_toolbar',
     'redisboard',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -138,3 +139,8 @@ INTERNAL_IPS = [
 CACHE_MIDDLEWARE_ALIAS = 'default'
 CACHE_MIDDLEWARE_SECONDS = 60 * 15 
 CACHE_MIDDLEWARE_KEY_PREFIX = 'educa'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+      'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+] }
